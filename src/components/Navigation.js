55c9a8251d15702de062de951cbcf8f15b484dce
Clinton6801 <akinwoleolaclinton@gmail.com>
2026-01-20 23:29:26 +0100

    errrormanagement

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import Link from 'next/link';
 import { ShoppingCart, Menu, X } from 'lucide-react';
 import { useCart } from '../context/CartContext';
-import { businessInfo } from '../../lib/data';
+import { businessInfo } from '../lib/data';
 
 export default function Navigation() {
   const [mobileMenu, setMobileMenu] = useState(false);
